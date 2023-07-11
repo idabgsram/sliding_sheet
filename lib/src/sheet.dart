121a8ced87ce396f20ecff55761fe2f446b49491
Gusram <idabgsram@gmail.com>
2023-07-11 22:36:17 +0800

    adjustment for newer sdk version

--- lib/src/sheet.dart
+++ lib/src/sheet.dart
@@ -1027,7 +1027,7 @@ class _SlidingSheetState extends State<SlidingSheet>
     if (scrollSpec.overscroll) {
       scrollView = GlowingOverscrollIndicator(
         axisDirection: AxisDirection.down,
-        color: scrollSpec.overscrollColor ?? Theme.of(context).accentColor,
+        color: scrollSpec.overscrollColor ?? Theme.of(context).colorScheme.secondary,
         child: scrollView,
       );
     }
